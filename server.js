d629eecdcc9a99df2c724f7e4d3439e00a427256
nayoung <rnjsskdud941@naver.com>
2020-01-16 15:29:16 +0900

    second commit

--- server.js
+++ server.js
@@ -31,24 +31,47 @@ http.createServer(function (request, response) {
     // response.writeHead(302, { 'Location': 'https://cs.dongduk.ac.kr' });
     // response.end();
 
-    
+
     //다양한 페이지 접근
-    var pathName = url.parse(request.url).pathname;
-    if (pathName == '/') {
-        fs.readFile('./html/index.html', function (error, data) {
+    // var pathName = url.parse(request.url).pathname;
+    // if (pathName == '/') {
+    //     fs.readFile('./html/index.html', function (error, data) {
+    //         response.writeHead(200, { 'Content-Type': 'text/html' });
+    //         response.end(data);
+    //     });
+    // }
+    // else if (pathName == '/second') {
+    //     fs.readFile('./html/second.html', function (error, data) {
+    //         response.writeHead(200, { 'Content-Type': 'text/html' });
+    //         response.end(data);
+    //     });
+    // }
+    // else if (pathName == '/cs') {
+    //     response.writeHead(302, { 'Location': 'https://cs.dongduk.ac.kr' });
+    //     response.end();
+    // }
+
+
+    //method 속성 구분(GET/POST)
+    // var queryString = require('querystring');
+
+    // var query = url.parse(request.url, true).query;
+    // response.writeHead(200, { 'Content-Type': 'text/html' });
+    // response.end('<h1>' + JSON.stringify(query) + '</h1>');
+
+    if (request.method == 'GET') {
+        fs.readFile('./html/login.html', function (error, data) {
             response.writeHead(200, { 'Content-Type': 'text/html' });
             response.end(data);
         });
     }
-    else if (pathName == '/second') {
-        fs.readFile('./html/second.html', function (error, data) {
+    else if (request.method == 'POST') {
+        request.on('data', function (data) {
             response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.end(data);
+            response.end('<h1>' + data + '</h1>');
+
         });
     }
-
-
-
 
 }).listen(1234, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1234');
